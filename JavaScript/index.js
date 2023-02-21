3c3e00e25f04b58d3b678aad1591105a8af95a49
KleemoffDeveloper <kleemoffdeveloper@gmail.com>
2023-02-21 14:36:55 -0500

    Changes :)

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -26,5 +26,13 @@ let hyperlinks =
 let hyperButtons = document.getElementsByClassName("pic-button");
 
 for(let i = 0; i < hyperButtons.length; i++){
-  hyperButtons[i].addEventListener("click", () => window.open(hyperlinks[i], "blank"));
+  if(hyperButtons[i])
+    hyperButtons[i].addEventListener("click", () => window.open(hyperlinks[i], "blank"));
 }
+
+// At this point I realize I shouldn't be using 1 JavaScript file for the whole website but... it's too late now LOL
+
+let linkedinPic = document.getElementById("linkedin-pic");
+
+if(linkedinPic)
+  linkedinPic.addEventListener("click", () => window.open("https://www.linkedin.com/in/silis-kleemoff/", "blank"));
